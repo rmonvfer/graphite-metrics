0dec2f4fef7752d17bb9ac172fd4265adfa4da44
David Anderson <david@pibcak.net>
2020-07-04 19:12:12 +0100

    Fixed a mistake

--- build/run-build.sh
+++ build/run-build.sh
@@ -7,12 +7,17 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
     --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
     .
   exit $?
+else
+  echo "Starting Master Build"
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+  TAG="${TRAVIS_TAG:-latest}"
+  VERSION_TAG="${IMAGE_VERSION}.${TRAVIS_BUILD_ID}"
+  docker buildx build \
+      --progress plain \
+      --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+      -t $DOCKER_REPO:$TAG \
+      -t $DOCKER_REPO:$VERSION_TAG \
+      --push \
+      .
+  exit $?
 fi
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin &> /dev/null
-TAG="${TRAVIS_TAG:-latest}"
-docker buildx build \
-     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
-    -t $DOCKER_REPO:$TAG \
-    --push \
-    .
